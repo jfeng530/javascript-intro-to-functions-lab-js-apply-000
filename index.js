da5d04285dbf97ba1726cf929d70c9dc4ec8ed6f
Jacky Feng <jackyfeng530@yahoo.com>
2019-07-23 18:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function sayHiToGrandma(string){
   if (string = lowercase) {
     return "I can't hear you!"
   }
-  if (string = uppercase) {
+  else if (string = uppercase) {
     return "YES INDEED!"
+  }
+  else if (string = "I love you, Grandma.") {
+    return "I love you, too."
+  }
   }
 }
