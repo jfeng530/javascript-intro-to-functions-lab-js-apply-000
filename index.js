9286f8968ec063d309e72faf571b288a54fb3378
Jacky Feng <jackyfeng530@yahoo.com>
2019-07-23 18:22:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,8 @@ function logWhisper(string) {
 var uppercase = "HELLO!"
 var lowercase = 'hello!'
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if (string = lowercase) {
     return "I can't hear you!"
-  }
-  else if (string = uppercase) {
-    return "YES INDEED!"
-  }
-  else if (string = "I love you, Grandma.") {
-    return "I love you, too."
   }
 }
