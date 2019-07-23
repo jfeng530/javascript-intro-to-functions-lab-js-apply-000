59e93a9553ac5912296fba9b622af5307fd1be4f
Jacky Feng <jackyfeng530@yahoo.com>
2019-07-23 18:29:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-var uppercase = "HELLO!"
-var lowercase = 'hello!'
+var uppercase = "HELLO"
+var lowercase = 'hello'
 
 function sayHiToGrandma(string) {
   switch (string) {
@@ -30,3 +30,18 @@ function sayHiToGrandma(string) {
       break;
   }
 }
+
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
